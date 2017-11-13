3ff5b6a94558fb3b089f32a86e8d5b8e4dc62337
krod <krod@github.com>
2017-11-13 00:02:07 -0200

    Parseando a string e atribuindo os valores pro objeto tanque

--- Tanque/Jogador.java
+++ Tanque/Jogador.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,9 +6,14 @@ import java.net.Socket;
 
 public class Jogador extends Thread{
 	private Socket socketJogador;
+	protected Tanque t;
+	private String stringArray[];
 
 	public Jogador(Socket socketJogador) {
 		this.socketJogador = socketJogador;
+		t = new Tanque(0, 0, 0, Color.BLACK);
+		Arena.tanques.add(t);
+		stringArray = new String[5];
 	}
 	
 	@Override
@@ -20,8 +26,13 @@ public class Jogador extends Thread{
 			while(true) {
 				//System.out.println("Recebendo...");
 				String s = reader.readLine();
-				if(s != null)
-					System.out.println(s);
+				if(s != null) {
+					//System.out.println(s);
+					stringArray = s.split(";");
+					t.x = Float.valueOf(stringArray[0]);
+					t.y = Float.valueOf(stringArray[1]);
+					t.angulo = Float.valueOf(stringArray[2]);
+				}
 			}
 			
 		} catch (IOException e) {
